308de2540f3f0f7d3f92625cc68ac17b0aa6a718
Auroali <dawn.auroali@gmail.com>
2024-10-13 19:17:31 -0300

    decayed twigs are now waterloggable

--- src/main/java/com/auroali/sanguinisluxuria/common/blocks/DecayedTwigsBlock.java
+++ src/main/java/com/auroali/sanguinisluxuria/common/blocks/DecayedTwigsBlock.java
@@ -1,26 +1,29 @@
 package com.auroali.sanguinisluxuria.common.blocks;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalFacingBlock;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
 import org.jetbrains.annotations.Nullable;
 
-public class DecayedTwigsBlock extends HorizontalFacingBlock {
+public class DecayedTwigsBlock extends HorizontalFacingBlock implements Waterloggable {
     public static final VoxelShape Z_SHAPE = Block.createCuboidShape(8, 0, 0, 9, 16, 16);
     public static final VoxelShape X_SHAPE = Block.createCuboidShape(0, 0, 8, 16, 16, 9);
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 
     public DecayedTwigsBlock(Settings settings) {
         super(settings);
-        this.setDefaultState(this.getStateManager().getDefaultState().with(FACING, Direction.NORTH));
+        this.setDefaultState(this.getStateManager().getDefaultState().with(FACING, Direction.NORTH).with(WATERLOGGED, false));
     }
 
     @Override
@@ -38,10 +41,16 @@ public class DecayedTwigsBlock extends HorizontalFacingBlock {
         return supportingState.isSideSolidFullSquare(world, supportingPos, state.get(FACING).getOpposite());
     }
 
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : super.getFluidState(state);
+    }
+
     @Nullable
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        BlockState state = this.getDefaultState();
+        BlockState state = this.getDefaultState()
+          .with(WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).isOf(Fluids.WATER));
         for (Direction direction : ctx.getPlacementDirections()) {
             if (direction.getAxis().isHorizontal()) {
                 state = state.with(FACING, direction);
@@ -50,6 +59,13 @@ public class DecayedTwigsBlock extends HorizontalFacingBlock {
             }
         }
         return null;
+    }
+
+    @Override
+    public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+        if (state.get(WATERLOGGED))
+            world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+        return super.getStateForNeighborUpdate(state, direction, neighborState, world, pos, neighborPos);
     }
 
     @Override
@@ -65,5 +81,6 @@ public class DecayedTwigsBlock extends HorizontalFacingBlock {
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         super.appendProperties(builder);
         builder.add(FACING);
+        builder.add(WATERLOGGED);
     }
 }
