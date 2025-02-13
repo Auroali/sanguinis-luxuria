09039a0b1f7f5b0867fd41a7aeb4a34a2f4a5947
Auroali <dawn.auroali@gmail.com>
2025-02-13 17:12:13 -0400

    wobble

--- src/main/java/com/auroali/sanguinisluxuria/client/render/effects/VampireHungerEffectManager.java
+++ src/main/java/com/auroali/sanguinisluxuria/client/render/effects/VampireHungerEffectManager.java
@@ -15,20 +15,24 @@ import net.minecraft.util.math.MathHelper;
 public class VampireHungerEffectManager {
     private static final ManagedShaderEffect SHADER = ShaderEffectManager.getInstance().manage(BLResources.VAMPIRE_HUNGER_SHADER);
     private static final Uniform1f PERCENT = SHADER.findUniform1f("Percent");
+    private static final Uniform1f RENDER_TIME = SHADER.findUniform1f("RenderTime");
     private static final int HUNGER_LIMIT = 6;
     private static final int MAX_TICKS = 10;
     private static final int MAX_TICKS_ENTITY = 20;
     private int ticks;
+    private int maxTicks;
+    private int renderTicks;
     private boolean render;
 
     public void tick(PlayerEntity entity) {
         HungerManager manager = entity.getHungerManager();
+        this.renderTicks++;
         if (manager.getFoodLevel() <= HUNGER_LIMIT) {
             this.render = true;
-            int maxTicks = this.shouldFadeIn() ? MAX_TICKS_ENTITY : MAX_TICKS;
-            if (this.ticks < maxTicks)
+            this.maxTicks = this.shouldFadeIn() ? this.getMaxTicksEntity(manager.getFoodLevel()) : this.getMaxTicks(manager.getFoodLevel());
+            if (this.ticks < this.maxTicks)
                 this.ticks++;
-            if (this.ticks > maxTicks)
+            if (this.ticks > this.maxTicks)
                 this.ticks--;
             return;
         }
@@ -41,12 +45,26 @@ public class VampireHungerEffectManager {
         if (!this.render && this.ticks == 0)
             return;
 
-        PERCENT.set(MathHelper.clamp(1.f - (this.ticks + tickDelta) / (float) MAX_TICKS, 0.2f, 1.f));
+        float renderTick = this.ticks == this.maxTicks
+          ? this.ticks
+          : this.ticks > this.maxTicks ? this.ticks - tickDelta : this.ticks + tickDelta;
+        RENDER_TIME.set((this.renderTicks + tickDelta) / 20.f);
+        PERCENT.set(MathHelper.clamp(1.f - renderTick / (float) MAX_TICKS_ENTITY, 0.3f, 1.f));
         SHADER.render(tickDelta);
+    }
+
+    public int getMaxTicks(int hunger) {
+        return MAX_TICKS - (int) (1.5f * hunger);
+    }
+
+    public int getMaxTicksEntity(int hunger) {
+        return MAX_TICKS_ENTITY - 2 * hunger;
     }
 
     public boolean shouldFadeIn() {
         HitResult result = MinecraftClient.getInstance().crosshairTarget;
-        return result != null && result.getType() == HitResult.Type.ENTITY && VampireHelper.hasBlood(((EntityHitResult) result).getEntity());
+        return result != null
+          && result.getType() == HitResult.Type.ENTITY
+          && VampireHelper.hasBlood(((EntityHitResult) result).getEntity());
     }
 }
--- src/main/resources/assets/sanguinisluxuria/shaders/program/vampire_hunger.fsh
+++ src/main/resources/assets/sanguinisluxuria/shaders/program/vampire_hunger.fsh
@@ -1,19 +1,46 @@
 #version 150
 
 uniform sampler2D DiffuseSampler;
-uniform float Time;
+uniform float RenderTime;
 uniform float Percent;
 
 in vec2 texCoord;
 
 out vec4 fragColor;
 
-const float PI = radians(180.0);
+const int ditherMatrix[16] = int[](
+1, 9, 3, 11,
+13, 5, 15, 7,
+4, 12, 2, 10,
+16, 8, 14, 6
+);
+
+// http://alex-charlton.com/posts/Dithering_on_the_GPU/
+float index() {
+    int x = int(mod(gl_FragCoord.x, 4));
+    int y = int(mod(gl_FragCoord.y, 4));
+    return ditherMatrix[(x + y * 4)] / 16.0;
+}
+
+float dither(float color) {
+    float closestColor = (color < 0.5) ? 0 : 1;
+    float secondClosest = 1 - closestColor;
+    float index = index();
+    float dist = abs(closestColor - color);
+    return (dist < index) ? closestColor : secondClosest;
+}
 
 void main() {
-    float time = 2.0 * PI * Time;
-    vec2 pos = vec2(texCoord.rg);
-    float gradient = clamp(length(pos - 0.5), 0.0, 1.0);
-    vec4 color = mix(texture2D(DiffuseSampler, texCoord), vec4(0.0), smoothstep(0.0, Percent, gradient));
+    vec2 pos = texCoord - 0.5;
+    float scroll = RenderTime / 8.0;
+    // add wobble
+    float xModifier = sin((scroll + pos.y) * 32.0) / 16.0;
+    float yModifier = cos((scroll + pos.x) * 16.0) / 16.0;
+    pos.x = pos.x + ((texCoord.x < 0.5) ? xModifier : -xModifier);
+    pos.y = pos.y + ((texCoord.y < 0.5) ? yModifier : -yModifier);
+    // generate gradient with wobble
+    float gradient = smoothstep(0.2, Percent, clamp(length(pos), 0.0, 1.0));
+    float ditherGradient = dither(gradient);
+    vec4 color = mix(texture2D(DiffuseSampler, texCoord), vec4(0.0), ditherGradient);
     fragColor = vec4(color.rgb, 1.0);
 }
