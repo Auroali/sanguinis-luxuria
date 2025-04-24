5f5079dfce73f44551425b16d822d37029bf468d
Auroali <dawn.auroali@gmail.com>
2025-04-24 17:22:23 -0300

    altar particle updated hopefully actually fixed altar reading/writing

--- src/main/java/com/auroali/sanguinisluxuria/common/rituals/ActiveRitualData.java
+++ src/main/java/com/auroali/sanguinisluxuria/common/rituals/ActiveRitualData.java
@@ -12,7 +12,9 @@ import net.minecraft.world.World;
 import java.util.UUID;
 
 public record ActiveRitualData(Ritual ritual, UUID initiator, UUID target) {
-    public static final String RITUAL_KEY = "active_ritual";
+    public static final String RITUAL_KEY = "ritual";
+    public static final String INITIATOR_KEY = "initiator";
+    public static final String TARGET_KEY = "target";
 
     public static void writeNbt(NbtCompound compound, ActiveRitualData data) {
         if (data == null)
@@ -20,21 +22,20 @@ public record ActiveRitualData(Ritual ritual, UUID initiator, UUID target) {
         Ritual.RITUAL_CODEC.encodeStart(NbtOps.INSTANCE, data.ritual())
           .resultOrPartial(Bloodlust.LOGGER::error)
           .ifPresent(element -> {
-              compound.putUuid("initiator", data.initiator);
-              compound.putUuid("target", data.target);
-              compound.put("ritual", element);
+              compound.putUuid(RITUAL_KEY, data.initiator);
+              compound.putUuid(INITIATOR_KEY, data.target);
+              compound.put(TARGET_KEY, element);
           });
     }
 
     public static ActiveRitualData readNbt(NbtCompound compound) {
-        if (!compound.contains(RITUAL_KEY, NbtElement.COMPOUND_TYPE))
+        if (!compound.contains(RITUAL_KEY))
             return null;
-        NbtCompound ritualTag = compound.getCompound(RITUAL_KEY);
-        return Ritual.RITUAL_CODEC.parse(NbtOps.INSTANCE, ritualTag.get("ritual"))
+        return Ritual.RITUAL_CODEC.parse(NbtOps.INSTANCE, compound.get(RITUAL_KEY))
           .resultOrPartial(Bloodlust.LOGGER::error)
           .map(ritual -> {
-              UUID initiator = ritualTag.getUuid("initiator");
-              UUID target = ritualTag.getUuid("target");
+              UUID initiator = compound.getUuid(INITIATOR_KEY);
+              UUID target = compound.getUuid(TARGET_KEY);
               return new ActiveRitualData(ritual, initiator, target);
           })
           .orElse(null);
