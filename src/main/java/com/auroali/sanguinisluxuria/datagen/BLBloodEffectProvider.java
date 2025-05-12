e76ef9129aa827698468438c0cbfd4675dd08835
Auroali <dawn.auroali@gmail.com>
2025-05-12 16:52:21 -0300

    optimized imports

--- src/main/java/com/auroali/sanguinisluxuria/datagen/BLBloodEffectProvider.java
+++ src/main/java/com/auroali/sanguinisluxuria/datagen/BLBloodEffectProvider.java
@@ -1,6 +1,7 @@
 package com.auroali.sanguinisluxuria.datagen;
 
 import com.auroali.sanguinisluxuria.BLResources;
+import com.auroali.sanguinisluxuria.common.blood.BloodDrainEffect;
 import com.auroali.sanguinisluxuria.common.blood.effects.BloodDrainIgniteEffect;
 import com.auroali.sanguinisluxuria.common.blood.effects.BloodDrainTeleportEffect;
 import com.auroali.sanguinisluxuria.common.registry.BLTags;
@@ -8,6 +9,7 @@ import com.auroali.sanguinisluxuria.datagen.builders.BloodDrainEffectBuilder;
 import com.auroali.sanguinisluxuria.datagen.generators.SanguinisLuxuriaBloodEffectsProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.resource.conditions.v1.DefaultResourceConditions;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.util.Identifier;
 
@@ -29,8 +31,18 @@ public class BLBloodEffectProvider extends SanguinisLuxuriaBloodEffectsProvider 
           .effect(new BloodDrainTeleportEffect(16, 0.8f))
           .offerTo(exporter, BLResources.id("teleport_on_drain"));
 
+        BloodDrainEffectBuilder.create(EntityType.WITCH)
+          .statusEffect(StatusEffects.WEAKNESS, 0.025f)
+          .statusEffect(StatusEffects.SLOWNESS, 0.025f)
+          .offerTo(exporter, BLResources.id("witch"));
+
+        this.generateCompat(exporter);
+    }
+
+    private void generateCompat(Consumer<BloodDrainEffectBuilder.Provider> exporter) {
         BloodDrainEffectBuilder.createOptional(new Identifier("spectrum", "kindling"))
           .effect(new BloodDrainIgniteEffect(8, 0.4f))
           .offerTo(withConditions(exporter, DefaultResourceConditions.allModsLoaded("spectrum")), BLResources.id("compat/spectrum/kindling_effects"));
+
     }
 }
