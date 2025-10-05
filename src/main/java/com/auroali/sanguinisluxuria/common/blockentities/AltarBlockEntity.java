e7ad5ed4e72038657519e65dd382907440acfba2
Auroali <dawn.auroali@gmail.com>
2025-10-05 17:35:40 -0300

    fancy altar effects

--- src/main/java/com/auroali/sanguinisluxuria/common/blockentities/AltarBlockEntity.java
+++ src/main/java/com/auroali/sanguinisluxuria/common/blockentities/AltarBlockEntity.java
@@ -9,6 +9,7 @@ import com.auroali.sanguinisluxuria.common.registry.*;
 import com.auroali.sanguinisluxuria.common.rituals.ActiveRitualData;
 import com.auroali.sanguinisluxuria.common.rituals.Ritual;
 import com.auroali.sanguinisluxuria.common.rituals.RitualParameters;
+import com.auroali.sanguinisluxuria.common.rituals.RitualUtil;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.advancement.criterion.Criteria;
@@ -29,15 +30,16 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.collection.DefaultedList;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.*;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.WorldChunk;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.function.Consumer;
 
 public class AltarBlockEntity extends BlockEntity implements Inventory, ItemDisplayingBlockEntity {
     private static final Vec3d ITEM_OFFSET = new Vec3d(0.5, 0.45, 0.5);
@@ -147,17 +149,13 @@ public class AltarBlockEntity extends BlockEntity implements Inventory, ItemDisp
         List<BlockPos> pedestalPositions = new ArrayList<>();
         List<ItemStack> pedestalItems = new ArrayList<>();
 
-        BlockPos.streamOutwards(pos, PEDESTAL_SEARCH_RADIUS, PEDESTAL_SEARCH_RADIUS, PEDESTAL_SEARCH_RADIUS)
-          .map(position -> world.getBlockEntity(position, SLBlockEntities.PEDESTAL))
-          .filter(Optional::isPresent)
-          .map(Optional::get)
-          .forEach(pedestal -> {
-              if (pedestal.getItem().isEmpty())
-                  return;
+        this.forEachPedestalAround(world, pos, pedestal -> {
+            if (pedestal.getItem().isEmpty())
+                return;
 
-              pedestalPositions.add(pedestal.getPos());
-              pedestalItems.add(pedestal.getItem().copy());
-          });
+            pedestalPositions.add(pedestal.getPos());
+            pedestalItems.add(pedestal.getItem().copy());
+        });
 
         SimpleInventory inventory = new SimpleInventory(pedestalItems.size() + 1);
         inventory.setStack(0, this.getStack(0));
@@ -190,6 +188,7 @@ public class AltarBlockEntity extends BlockEntity implements Inventory, ItemDisp
                       world.spawnEntity(itemEntity);
                   }
                   entity.getInventory().markDirty();
+                  RitualUtil.spawnItemConsumedParticlesAt(world, position);
               });
 
               this.ticksProcessing = 0;
@@ -206,6 +205,28 @@ public class AltarBlockEntity extends BlockEntity implements Inventory, ItemDisp
               }
               this.clearNextTarget();
           });
+    }
+
+    protected void forEachPedestalAround(World world, BlockPos pos, Consumer<? super PedestalBlockEntity> consumer) {
+        BlockPos min = new BlockPos(pos.getX() - PEDESTAL_SEARCH_RADIUS, pos.getY() - PEDESTAL_SEARCH_RADIUS, pos.getZ() - PEDESTAL_SEARCH_RADIUS);
+        BlockPos max = new BlockPos(pos.getX() + PEDESTAL_SEARCH_RADIUS, pos.getY() + PEDESTAL_SEARCH_RADIUS, pos.getZ() + PEDESTAL_SEARCH_RADIUS);
+        Box bounds = new Box(min, max);
+        int chunkMinX = ChunkSectionPos.getSectionCoord(min.getX());
+        int chunkMaxX = ChunkSectionPos.getSectionCoord(max.getX());
+        int chunkMinZ = ChunkSectionPos.getSectionCoord(min.getZ());
+        int chunkMaxZ = ChunkSectionPos.getSectionCoord(max.getZ());
+
+        for (int x = chunkMinX; x <= chunkMaxX; x++) {
+            for (int z = chunkMinZ; z <= chunkMaxZ; z++) {
+                if (!world.isChunkLoaded(x, z))
+                    continue;
+                WorldChunk chunk = world.getChunk(x, z);
+                chunk.getBlockEntities().forEach((bPos, be) -> {
+                    if (be instanceof PedestalBlockEntity pedestalBlockEntity && bounds.contains(bPos.getX(), bPos.getY(), bPos.getZ()))
+                        consumer.accept(pedestalBlockEntity);
+                });
+            }
+        }
     }
 
     public LivingEntity getInitiator() {
--- src/main/java/com/auroali/sanguinisluxuria/common/rituals/RitualUtil.java
+++ src/main/java/com/auroali/sanguinisluxuria/common/rituals/RitualUtil.java
@@ -5,7 +5,9 @@ import com.auroali.sanguinisluxuria.common.registry.SLParticles;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 
 public class RitualUtil {
     public static void spawnSuccessParticles(RitualParameters parameters) {
@@ -28,6 +30,28 @@ public class RitualUtil {
               100
             );
             PlayerLookup.tracking(world, parameters.pos())
+              .forEach(player -> ServerPlayNetworking.send(
+                player, packet
+              ));
+        }
+    }
+
+    public static void spawnItemConsumedParticlesAt(World world, BlockPos pos) {
+        if (world instanceof ServerWorld serverWorld) {
+            EmitParticlesS2C packet = new EmitParticlesS2C(
+              SLParticles.ALTAR,
+              pos.getX() + 0.5,
+              pos.getY() + 0.5,
+              pos.getZ() + 0.5,
+              0.f,
+              0.f,
+              0.f,
+              0.05,
+              0.02f,
+              0.05f,
+              25
+            );
+            PlayerLookup.tracking(serverWorld, pos)
               .forEach(player -> ServerPlayNetworking.send(
                 player, packet
               ));
