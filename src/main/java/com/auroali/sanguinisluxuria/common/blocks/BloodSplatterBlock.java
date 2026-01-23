Auroali/sanguinis-luxuria

commit 48d244690146a5e8062b065b14448265e6a8f039

most methods of filling/draining blood-storing items now accounts for stackable items

src/main/java/com/auroali/sanguinisluxuria/common/blocks/BloodSplatterBlock.java
@@ -125,35 +125,26 @@ public class BloodSplatterBlock extends Block {
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        // todo: cleanup
-        ItemStack stack = player.getStackInHand(hand);
-        boolean canBeFilled = BloodStorageFillEvents.ALLOW_ITEM.invoker().allowItem(player, stack);
-        if (!canBeFilled)
-            return ActionResult.FAIL;
-
-        ItemStack resultItem = stack;
-        if (!BloodStorageItem.isItemFillable(stack)) {
-            resultItem = BloodStorageFillEvents.TRANSFORM_STACK.invoker().createFrom(player, stack);
-            if (!BloodStorageItem.isItemFillable(resultItem) || BloodStorageItem.getItemCapacity(resultItem) < BloodConstants.BLOOD_PER_BOTTLE)
-                return ActionResult.FAIL;
-
-            stack.decrement(1);
+        ItemStack held = player.getStackInHand(hand);
+        ItemStack originalHeld = held.copy();
+        ItemStack toFill = held.split(1);
+        if (!(held.getItem() instanceof BloodStorageItem) && BloodStorageFillEvents.ALLOW_ITEM.invoker().allowItem(player, toFill)) {
+            toFill = BloodStorageFillEvents.TRANSFORM_STACK
+              .invoker().createFrom(player, toFill);
         }
 
-        if (BloodStorageItem.getItemCapacity(resultItem) < BloodConstants.BLOOD_PER_BOTTLE)
+        if (!BloodStorageItem.isItemFillable(toFill) || !BloodStorageItem.incrementItemBlood(toFill, BloodConstants.BLOOD_PER_BOTTLE)) {
+            player.setStackInHand(hand, originalHeld);
             return ActionResult.FAIL;
-
-        BloodStorageItem.incrementItemBlood(resultItem, BloodConstants.BLOOD_PER_BOTTLE);
-
-        world.removeBlock(pos, false);
-        world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.PLAYERS, 1.0f, 1.0f);
-
-        if (stack != resultItem) {
-            if (stack.isEmpty())
-                player.setStackInHand(hand, resultItem);
-            else if (!player.getInventory().insertStack(resultItem))
-                player.dropItem(resultItem, true);
         }
+
+        if (held.isEmpty()) {
+            player.setStackInHand(hand, toFill);
+            return ActionResult.success(world.isClient);
+        }
+
+        if (!player.getInventory().insertStack(toFill))
+            player.dropItem(toFill, false);
 
         return ActionResult.success(world.isClient);
     }

src/main/java/com/auroali/sanguinisluxuria/VampireHelper.java
@@ -251,23 +251,21 @@ public class VampireHelper {
      * @return the amount of blood successfully filled
      */
     public static int fillHeldBloodStorage(LivingEntity entity, ItemStack stack, Hand hand, int amount, Consumer<ItemStack> consumer) {
-        ItemStack resultStack = stack;
+        ItemStack original = stack.copy();
+        ItemStack resultStack = stack.split(1);
         if (!(resultStack.getItem() instanceof BloodStorageItem)) {
             resultStack = BloodStorageFillEvents.TRANSFORM_STACK.invoker().createFrom(entity, resultStack);
         }
 
         int amountToFill = Math.min(amount, BloodStorageItem.getItemCapacity(resultStack));
 
-        if (amountToFill == 0 || !BloodStorageItem.isItemFillable(resultStack) || !BloodStorageItem.incrementItemBlood(resultStack, amountToFill))
+        if (amountToFill == 0 || !BloodStorageItem.isItemFillable(resultStack) || !BloodStorageItem.incrementItemBlood(resultStack, amountToFill)) {
+            entity.setStackInHand(hand, original);
             return 0;
+        }
 
         if (consumer != null)
             consumer.accept(resultStack);
-
-        if (stack == resultStack)
-            return amountToFill;
-
-        stack.decrement(1);
 
         if (stack.isEmpty()) {
             entity.setStackInHand(hand, resultStack);

src/main/java/com/auroali/sanguinisluxuria/common/registry/SLCauldronBehaviours.java
@@ -1,62 +1,40 @@
 package com.auroali.sanguinisluxuria.common.registry;
 
+import com.auroali.sanguinisluxuria.common.blocks.BloodCauldronBlock;
 import com.auroali.sanguinisluxuria.common.blood.BloodConstants;
 import com.auroali.sanguinisluxuria.common.items.BloodStorageItem;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.LeveledCauldronBlock;
 import net.minecraft.block.cauldron.CauldronBehavior;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
 import net.minecraft.item.Items;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 
 import java.util.Map;
 
 public class SLCauldronBehaviours {
     public static final Map<Item, CauldronBehavior> BLOOD_CAULDRON_BEHAVIOUR = CauldronBehavior.createMap();
     public static final CauldronBehavior BLOOD_STORING_ITEM_FILL = (state, world, pos, player, hand, stack) -> {
-        if (stack.getItem() instanceof BloodStorageItem) {
-            if (BloodStorageItem.getItemBlood(stack) >= BloodConstants.BLOOD_PER_BOTTLE) {
-                int bloodToDrain = Math.min(BloodStorageItem.getItemBlood(stack) / BloodConstants.BLOOD_PER_BOTTLE, LeveledCauldronBlock.MAX_LEVEL);
-                BloodStorageItem.decrementItemBlood(stack, bloodToDrain * BloodConstants.BLOOD_PER_BOTTLE);
-                if (BloodStorageItem.isItemEmpty(stack)) {
-                    ItemStack emptyStack = BloodStorageItem.createEmptyStackFor(stack);
-                    if (emptyStack != stack)
-                        player.setStackInHand(hand, emptyStack);
-                }
-                world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ITEM_BOTTLE_EMPTY, SoundCategory.BLOCKS, 1, 1);
-                world.setBlockState(pos, SLBlocks.BLOOD_CAULDRON.getDefaultState().with(LeveledCauldronBlock.LEVEL, bloodToDrain));
-                return ActionResult.success(world.isClient);
-            }
-        }
+        if (fillCauldronFrom(state, world, pos, player, hand, stack))
+            return ActionResult.success(world.isClient);
         return ActionResult.FAIL;
     };
     public static final CauldronBehavior BLOOD_STORING_ITEM_DRAIN_FILL = (state, world, pos, player, hand, stack) -> {
-        if (stack.getItem() instanceof BloodStorageItem) {
-            if (state.get(LeveledCauldronBlock.LEVEL) >= 1 && BloodStorageItem.getItemCapacity(stack) >= BloodConstants.BLOOD_PER_BOTTLE) {
-                BloodStorageItem.incrementItemBlood(stack, BloodConstants.BLOOD_PER_BOTTLE);
-
-                LeveledCauldronBlock.decrementFluidLevel(state, world, pos);
-                world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.BLOCKS, 1, 1);
-                return ActionResult.success(world.isClient);
-            }
-
-            int level = state.contains(LeveledCauldronBlock.LEVEL) ? state.get(LeveledCauldronBlock.LEVEL) : 0;
-            int newLevel = Math.min(level + BloodStorageItem.getItemBlood(stack) / BloodConstants.BLOOD_PER_BOTTLE, LeveledCauldronBlock.MAX_LEVEL);
-            if (level == newLevel)
-                return ActionResult.FAIL;
-
-            BloodStorageItem.decrementItemBlood(stack, (newLevel - level) * BloodConstants.BLOOD_PER_BOTTLE);
-            if (BloodStorageItem.isItemEmpty(stack)) {
-                ItemStack emptyStack = BloodStorageItem.createEmptyStackFor(stack);
-                if (emptyStack != stack)
-                    player.setStackInHand(hand, emptyStack);
-            }
-            world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ITEM_BOTTLE_EMPTY, SoundCategory.BLOCKS, 1, 1);
-            world.setBlockState(pos, SLBlocks.BLOOD_CAULDRON.getDefaultState().with(LeveledCauldronBlock.LEVEL, newLevel));
+        if (fillFromCauldron(state, world, pos, player, hand, stack))
             return ActionResult.success(world.isClient);
-        }
+        if (fillCauldronFrom(state, world, pos, player, hand, stack))
+            return ActionResult.success(world.isClient);
         return ActionResult.FAIL;
     };
 
@@ -64,11 +42,10 @@ public class SLCauldronBehaviours {
         if (stack.isOf(Items.GLASS_BOTTLE)) {
             ItemStack bloodBottle = BloodStorageItem.createStack(SLItems.BLOOD_BOTTLE, BloodConstants.BLOOD_PER_BOTTLE);
             LeveledCauldronBlock.decrementFluidLevel(state, world, pos);
-            stack.decrement(1);
-            if (stack.isEmpty())
-                player.setStackInHand(hand, bloodBottle);
-            else if (!player.getInventory().insertStack(bloodBottle))
-                player.dropItem(bloodBottle, false);
+            player.incrementStat(Stats.FILL_CAULDRON);
+            player.incrementStat(Stats.USED.getOrCreateStat(stack.getItem()));
+            world.playSound(null, pos, SoundEvents.ITEM_BOTTLE_EMPTY, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            player.setStackInHand(hand, ItemUsage.exchangeStack(stack, player, bloodBottle));
             world.playSound(null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.BLOCKS, 1, 1);
             return ActionResult.success(world.isClient);
         }
@@ -81,5 +58,53 @@ public class SLCauldronBehaviours {
         BLOOD_CAULDRON_BEHAVIOUR.put(SLItems.BLOOD_BOTTLE, BLOOD_STORING_ITEM_DRAIN_FILL);
         BLOOD_CAULDRON_BEHAVIOUR.put(SLItems.BLOOD_BAG, BLOOD_STORING_ITEM_DRAIN_FILL);
         BLOOD_CAULDRON_BEHAVIOUR.put(Items.GLASS_BOTTLE, FILL_GLASS_BOTTLE);
+    }
+
+    public static boolean fillFromCauldron(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, ItemStack stack) {
+        if (BloodStorageItem.isItemFillable(stack) && BloodStorageItem.getItemCapacity(stack) >= BloodConstants.BLOOD_PER_BOTTLE) {
+            ItemStack filled = stack.split(1);
+            BloodStorageItem.incrementItemBlood(filled, BloodConstants.BLOOD_PER_BOTTLE);
+            BloodCauldronBlock.decrementFluidLevel(state, world, pos);
+            if (stack.isEmpty()) {
+                player.setStackInHand(hand, filled);
+            } else if (!player.getInventory().insertStack(filled)) {
+                player.dropItem(filled, false);
+            }
+            player.incrementStat(Stats.USE_CAULDRON);
+            player.incrementStat(Stats.USED.getOrCreateStat(filled.getItem()));
+            world.playSound(null, pos, SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            world.emitGameEvent(null, GameEvent.FLUID_PICKUP, pos);
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean fillCauldronFrom(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, ItemStack stack) {
+        if (
+          BloodStorageItem.isItemDrainable(stack)
+            && BloodStorageItem.getItemBlood(stack) >= BloodConstants.BLOOD_PER_BOTTLE
+            && (state.isOf(Blocks.CAULDRON) || state.get(LeveledCauldronBlock.LEVEL) < 3)
+        ) {
+            ItemStack drained = stack.split(1);
+            BloodStorageItem.decrementItemBlood(stack, BloodConstants.BLOOD_PER_BOTTLE);
+            world.setBlockState(pos, state.isOf(Blocks.CAULDRON)
+              ? SLBlocks.BLOOD_CAULDRON.getDefaultState().with(LeveledCauldronBlock.LEVEL, 1)
+              : state.with(LeveledCauldronBlock.LEVEL, state.get(LeveledCauldronBlock.LEVEL) + 1)
+            );
+            if (BloodStorageItem.isItemEmpty(drained)) {
+                drained = BloodStorageItem.createEmptyStackFor(drained);
+            }
+            if (stack.isEmpty()) {
+                player.setStackInHand(hand, drained);
+            } else if (!player.getInventory().insertStack(drained)) {
+                player.dropItem(drained, false);
+            }
+            player.incrementStat(Stats.FILL_CAULDRON);
+            player.incrementStat(Stats.USED.getOrCreateStat(drained.getItem()));
+            world.playSound(null, pos, SoundEvents.ITEM_BOTTLE_EMPTY, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            world.emitGameEvent(null, GameEvent.FLUID_PLACE, pos);
+            return true;
+        }
+        return false;
     }
 }

commit 000b5a27b82a4b1ee1b659f77c6de819ffc4d79f

fix issues related to picking up/placing blood introduced in 48d2446

src/main/java/com/auroali/sanguinisluxuria/common/blocks/BloodSplatterBlock.java
@@ -14,7 +14,10 @@ import net.minecraft.block.enums.WireConnection;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.EnumProperty;
@@ -31,6 +34,7 @@ import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import net.minecraft.world.WorldView;
+import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
@@ -124,25 +128,25 @@ public class BloodSplatterBlock extends Block {
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         ItemStack held = player.getStackInHand(hand);
-        ItemStack originalHeld = held.copy();
-        ItemStack toFill = held.split(1);
+        ItemStack toFill = held.copyWithCount(1);
         if (!(held.getItem() instanceof BloodStorageItem) && BloodStorageFillEvents.ALLOW_ITEM.invoker().allowItem(player, toFill)) {
             toFill = BloodStorageFillEvents.TRANSFORM_STACK
               .invoker().createFrom(player, toFill);
         }
 
         if (!BloodStorageItem.isItemFillable(toFill) || !BloodStorageItem.incrementItemBlood(toFill, BloodConstants.BLOOD_PER_BOTTLE)) {
-            player.setStackInHand(hand, originalHeld);
             return ActionResult.FAIL;
         }
 
-        if (held.isEmpty()) {
-            player.setStackInHand(hand, toFill);
-            return ActionResult.success(world.isClient);
+        if (!world.isClient) {
+            player.incrementStat(Stats.USED.getOrCreateStat(held.getItem()));
         }
 
-        if (!player.getInventory().insertStack(toFill))
-            player.dropItem(toFill, false);
+        world.removeBlock(pos, false);
+        world.playSound(player, pos, SoundEvents.ITEM_BOTTLE_FILL, player.getSoundCategory(), 1.f, 1.f);
+        world.emitGameEvent(player, GameEvent.FLUID_PICKUP, pos);
+
+        player.setStackInHand(hand, ItemUsage.exchangeStack(held, player, toFill));
 
         return ActionResult.success(world.isClient);
     }

src/main/java/com/auroali/sanguinisluxuria/common/items/DrinkableBloodItem.java
@@ -159,7 +159,7 @@ public class DrinkableBloodItem extends Item implements BloodStorageItem, Entity
             ItemStack drained = stack.split(1);
             BloodStorageItem.decrementItemBlood(drained, BloodConstants.BLOOD_PER_BOTTLE);
             if (BloodStorageItem.isItemEmpty(drained))
-                drained = BloodStorageItem.createEmptyStackFor(stack);
+                drained = BloodStorageItem.createEmptyStackFor(drained);
 
             if (stack.isEmpty())
                 player.setStackInHand(context.getHand(), drained);